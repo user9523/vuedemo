f699c2f6fd21efaef451c6104be408aaba14ca8d
Alay <ylhmx80@aliyun.com>
2020-12-15 14:47:34 +0800

    store 更新网页标题方法

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,6 @@
 
-import { createApp } from 'vue'
 import { createStore } from 'vuex'
+import { updatelocal } from '../utils/utils'
  
  
 // 创建 store 容器实例.
@@ -17,9 +17,42 @@ const store = createStore({
         else if(key === 'catalogue'){
             const catalogue = state.catalogue
             let length1 = catalogue.length
+            for (let i = 0; i < length1; i++) {
+                let currennt  = catalogue[i]
+                if(currennt.id == value.id.split('.')[0]){
+                    // 修改的是标签信息
+                    if(currennt.id == value.id){
+                        currennt.name = value.name
+                        currennt.icon = value.icon
+                        updatelocal(store)
+                    } else {
+                        let URLS =  currennt.URLS
+                        let length2 = currennt.length
+                        for (let j = 0; j < length2; j++) {
+                            const URL = URLS[j];
+                            if(URL.id == value.id){
+                                URL.url = value.url
+                                URL.name = value.name
+                                URL.icon = value.icon
+                                updatelocal(store)
+                                break
+                            }
+                        }
+                    }
+                    break
+                }
+                
+            }
         }
-       }
+       },
+       // 移除(根据id来移除)
+
     }
+    // modules: {
+    //     moduleTab,
+    //     moduleUrl,
+    //     moduleSearch
+    // }
 })
  
  
